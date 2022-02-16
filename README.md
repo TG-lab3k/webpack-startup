db9b3cc211c81fc5305af4375e6a411a92ab23da
Leiguoting <leigt3@outlook.com>
2022-02-16 14:40:52 +0800

    管理资源

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-Webpack起步学习
-官方文档
+## Webpack起步学习
+#### 官方文档
 https://webpack.docschina.org/guides/getting-started/
 
-1、Webpack起步
+#### 1、Webpack起步
+
+#### 2、资源管理
+* 加载css
+
+* 加载images图像
+
+* 加载fonts字体
+
+* 加载数据
+  
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './ic_test1.png';
+import csvData from './data.csv';
+import xmlData from './data.xml';
 
 function component() {
     const element = document.createElement('div');
 
     // lodash 在当前 script 中使用 import 引入
     element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+    element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    const xmlEle = document.createElement('div');
+    xmlEle.innerHTML = xmlData;
+    element.appendChild(xmlEle)
+
+    const csvEle = document.createElement('div');
+    csvEle.innerHTML = _.join(csvData, ' ');
+    element.appendChild(csvEle);
+
     return element;
 }
 document.body.appendChild(component());
